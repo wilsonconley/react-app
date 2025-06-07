80bcc615af64c81226670fce597ac236b2dc7610
wilsonconley <conleywilson0@gmail.com>
2025-06-07 12:29:37 -0400

    Basic React app

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const items = ["New York", "Paris", "Chicago"];
+
+  const [showAlert, setShowAlert] = useState(false);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={(item) => console.log(item)}
+      />
+      {showAlert && (
+        <Alert onClose={() => setShowAlert(false)}>
+          <strong>Holy guacamole!</strong> You should check in on some of those
+          fields below.
+        </Alert>
+      )}
+      <Button color="secondary" onClick={() => setShowAlert(true)}>
+        Show Alert
+      </Button>
+    </div>
+  );
 }
 
-export default App
+export default App;
